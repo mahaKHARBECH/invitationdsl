aebc9709e6568c16e2652e9959c89f84d08fabaa
MahaKh <maha.kharbech@gmail.com>
2016-12-12 13:43:10 -0500

    Supprimer les redondances

--- invitationdsl/src/invitationdsl/Invitation.java
+++ invitationdsl/src/invitationdsl/Invitation.java
@@ -9,11 +9,11 @@ public class Invitation {
 
 			
 		private String nomInvite;
-		private String nomConference;
-		private String lieuConference;
+		private String nom;
+		private String lieu;
 		private String[] conferenciers;
-		private String themeConference;
-		private Date dateConference;
+		private String theme;
+		private Date date;
 		
 		
 		protected void nomInvite( String nomInv ) { nomInvite = nomInv; }
@@ -22,22 +22,22 @@ public class Invitation {
 			conferenciers = confs.clone();
 		}
 	
-		protected void nomConference( String nomConf ) {
-			nomConference = nomConf;
+		protected void nom( String nomConf ) {
+			nom = nomConf;
 		}
 		
-		protected void themeConference( String themeConf ) {
-			themeConference = themeConf;
+		protected void theme( String themeConf ) {
+			theme = themeConf;
 		}
 		
-		protected void lieuConference( String lieuConf ) {
-			lieuConference = lieuConf;
+		protected void lieu( String lieuConf ) {
+			lieu = lieuConf;
 		}
-		protected void dateConference( String dateInString ) {
+		protected void date( String dateInString ) {
 			
 			SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
 			try {
-				dateConference = format1.parse( dateInString );
+				date = format1.parse( dateInString );
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
@@ -48,37 +48,37 @@ public class Invitation {
 			d.build( this );
 			}
 		
-		public static void printInvitation(Invitation inv)
+		public static void print(Invitation inv)
 		{
 			System.out.println("---------------Invitation---------------");
 			System.out.println("Nom de l'invite : "+inv.nomInvite);
-			System.out.println("Nom de la conference : "+inv.nomConference);
-			System.out.println("Theme de la conference : "+inv.themeConference);
-			System.out.println("Date de la conference : "+inv.dateConference);
-			System.out.println("Lieu de la conference : "+inv.lieuConference);
+			System.out.println("Nom de la conference : "+inv.nom);
+			System.out.println("Theme de la conference : "+inv.theme);
+			System.out.println("Date de la conference : "+inv.date);
+			System.out.println("Lieu de la conference : "+inv.lieu);
 			for (String c : inv.conferenciers){
 			System.out.println("Conferenciers : "+c);
 			}
 		}
 		
 		
-		public String NomInvite() {
+		public String nomInvite() {
 			return nomInvite;
 		}
-		public String NomConference() {
-			return nomConference;
+		public String nom() {
+			return nom;
 		}
-		public String LieuConference() {
-			return lieuConference;
+		public String lieu() {
+			return lieu;
 		}
-		public String[] Conferenciers() {
+		public String[] conferenciers() {
 			return conferenciers;
 		}
-		public String ThemeConference() {
-			return themeConference;
+		public String theme() {
+			return theme;
 		}
-		public Date DateConference() {
-			return dateConference;
+		public Date date() {
+			return date;
 		}
 		
 		
--- invitationdsl/src/invitationdsl/Main.java
+++ invitationdsl/src/invitationdsl/Main.java
@@ -5,14 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		Invitation invitation = new Invitation( inv -> {
 			inv.nomInvite("James Bond");
-			inv.nomConference("Java One");
-			inv.themeConference("Internal DSL in Java 8");
-			inv.lieuConference("Montreal - CANADA");
+			inv.nom("Java One");
+			inv.theme("Internal DSL in Java 8");
+			inv.lieu("Montreal - CANADA");
 			inv.conferenciers("M. Charles Nutter, M. James Ward"); 
-			inv.dateConference("30-03-2017 09:00:00");
+			inv.date("30-03-2017 09:00:00");
 			} );
 
-		Invitation.printInvitation(invitation);
+		Invitation.print(invitation);
 	}
 
 }
